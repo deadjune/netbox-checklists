51aee8e5bb6bb70b90a1b6027f21fa38aa396991
Anna Krymina <aakrymina@ct-dc.ru>
2022-01-27 17:22:26 +0300

    add setup.py, __init__.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-checklists',
+    version='0.1',
+    description='Netbox plugin for creating checklists',
+    url='',
+    author='Anna Krymina',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
