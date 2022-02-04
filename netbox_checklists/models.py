120586b58fabb823581a1ac4fd68f83e9e9db626
Anna Krymina <aakrymina@ct-dc.ru>
2022-02-04 07:10:39 +0300

    fix models.py

--- netbox_checklists/models.py
+++ netbox_checklists/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+from django.contrib.contenttypes.models import ContentType
+
 class Checklist(models.Model):
     checklist_name = models.CharField(max_length=100) #human-readable name
     related_object_types = models.ManyToManyField(
         to=ContentType
     )
+
+    def __str__(self):
+        return self.checklist_name, self.related_object_types
