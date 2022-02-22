5a8a701e2378fa61a370645583dea70fcea139ea
Anna Krymina <aakrymina@ct-dc.ru>
2022-02-22 14:22:56 +0300

    test list_display

--- netbox_checklists/admin.py
+++ netbox_checklists/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Checklist
 
-@admin.register(Checklist)
+#@admin.register(Checklist)
 class ChecklistAdmin(admin.ModelAdmin):
-    list_display = ('checklist_name',)
+    #list_display = ('checklist_name',)
+    #pass
+admin.site.register(Checklist, ChecklistAdmin)
