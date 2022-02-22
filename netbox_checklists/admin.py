d0d459f0cdd709c564729c8a6b4b92873653a430
Anna Krymina <aakrymina@ct-dc.ru>
2022-02-22 08:53:03 +0300

    fix typo

--- netbox_checklists/admin.py
+++ netbox_checklists/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Checklist
 
 @admin.register(Checklist)
-class AnimalAdmin(admin.ModelAdmin):
+class ChecklistAdmin(admin.ModelAdmin):
     list_display = ('checklist_name')
